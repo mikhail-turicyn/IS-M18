0914a7e3bd361d8f566d0030039c3312fa089eb1
CDIY <connect@obninsk.ru>
2017-05-16 13:02:31 +0300

    Убрал статичность объектов player Исправил баг с временем

--- core/src/edu/isb14/game/MainMenuScreen.java
+++ core/src/edu/isb14/game/MainMenuScreen.java
@@ -137,6 +137,7 @@ public class MainMenuScreen implements Screen {
             // Quit
             if (currentItem == 4) {
                 Gdx.app.exit();
+                game.dispose();
             }
         }
 
