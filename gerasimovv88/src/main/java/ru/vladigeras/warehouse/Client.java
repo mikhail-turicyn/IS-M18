bd61f70c6826c9b6e0571f3ec7e6de342a020415
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-10-31 20:08:49 +0300

    Init a project: small system for managing warehouse (store).

---
+++ gerasimovv88/src/main/java/ru/vladigeras/warehouse/Client.java
@@ -0,0 +1,11 @@
+package ru.vladigeras.warehouse;
+
+/**
+ * @author vladi_geras on 31/10/2018
+ */
+public class Client {
+	public static void main(String[] args) {
+		//TODO: client application and system entry point
+		System.out.println("Hello World");
+	}
+}
