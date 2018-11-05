48b4f2bbf0a2d07fe686c778ad699ec066b78df1
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-11-05 21:31:09 +0300

    Client side of application: running a warehouse with examples

--- gerasimovv88/src/main/java/ru/vladigeras/warehouse/Client.java
+++ gerasimovv88/src/main/java/ru/vladigeras/warehouse/Client.java
@@ -1,11 +1,41 @@
 package ru.vladigeras.warehouse;
 
+import java.util.Random;
+
 /**
+ * Сlient application and system entry point
  * @author vladi_geras on 31/10/2018
  */
 public class Client {
 	public static void main(String[] args) {
-		//TODO: client application and system entry point
-		System.out.println("Hello World");
+		System.out.println("------------- Warehouse initializing ------------");
+
+		var warehouse = gadgetWarehouseInit();
+
+		System.out.println();
+
+		warehouse.printItems();
+
+		System.out.println();
+
+		warehouse = deleteItem(warehouse);
+		warehouse.printItems();
+	}
+
+	private static Warehouse gadgetWarehouseInit() {
+		var gadgetWarehouse = new GadgetWarehouse(1L, "Gadget warehouse");
+		gadgetWarehouse.addItem(new Gadget(1L, "Smartphone", 18.990, "Apple IPhone SE"));
+		gadgetWarehouse.addItem(new Gadget(2L, "Smartphone", 37.990, "Apple IPhone 7"));
+		gadgetWarehouse.addItem(new Gadget(3L, "Smartphone", 62.000, "Apple MacBook Air"));
+
+		return gadgetWarehouse;
+	}
+
+	private static Warehouse deleteItem(Warehouse warehouse) {
+		var randomInt = new Random().nextInt(3) + 1;
+		System.out.println("Deleted id is " + randomInt + " now");
+
+		warehouse.removeItem(warehouse.getItems().get(randomInt - 1));
+		return warehouse;
 	}
 }
