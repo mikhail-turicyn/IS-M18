419dc708a9b796c6789bf6b9ea0528a27c0fd93f
AleksKuzn <a_v_k_07@list.ru>
2018-11-16 13:12:59 +0300

    добавил форму newJFrame

--- src/photoeditor/PhotoEditor.java
+++ src/photoeditor/PhotoEditor.java
@@ -3,19 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package photoeditor;
+package anaglyph3d;
 
-/**
- *
- * @author Alexander
- */
-public class PhotoEditor {
+public class Anaglyph3D {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new NewJFrame().setVisible(true);
     }
     
 }
