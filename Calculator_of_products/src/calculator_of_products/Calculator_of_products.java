60baa86c934177ee4975c89e892825bd1b184ca4
Katherine <43581502+tuxa95@users.noreply.github.com>
2018-11-22 13:21:42 +0300

    Add code
    Calculator ver 2.0

--- Calculator_of_products/src/calculator_of_products/Calculator_of_products.java
+++ Calculator_of_products/src/calculator_of_products/Calculator_of_products.java
@@ -4,18 +4,29 @@
  * and open the template in the editor.
  */
 package calculator_of_products;
-
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author Катюша
  */
-public class Calculator_of_products {
+public class Calculator_of_products extends DefaultTableModel {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Calculator_of_products() {
+        super(new String[]{"Название", "Цена за шт.", "Кол-во", "Стоимость"}, 1); //названия столбцов
+        super.setValueAt("Итого:", getRowCount() - 1, 2);
+        super.setValueAt("0.0", getRowCount() - 1, 3);
+
+    }
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        if (column == 0 && row != getRowCount() - 1) {
+            return true;
+        } else {
+            return false;//запрет редактирования ячеек
+        }
     }
     
 }
