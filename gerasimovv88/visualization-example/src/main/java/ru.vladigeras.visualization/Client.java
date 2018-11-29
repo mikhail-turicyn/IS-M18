e93eff51a142618bc5064606cbbc8214e876bd5b
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2018-11-29 14:58:21 +0300

    JFreeChart library example: Bar Chart with each description bar

---
+++ gerasimovv88/visualization-example/src/main/java/ru.vladigeras.visualization/Client.java
@@ -0,0 +1,12 @@
+package ru.vladigeras.visualization;
+
+/**
+ * @author vladi_geras on 2018-11-29
+ */
+public class Client {
+	public static void main(String[] args) {
+		var demoJFreeChartBarChart = new JFreeChartExample("Bar Chart");
+		demoJFreeChartBarChart.pack();
+		demoJFreeChartBarChart.setVisible(true);
+	}
+}
